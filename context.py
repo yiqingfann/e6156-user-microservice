fb9ad77aa2be36e43d1f4957e7df6c01abe6cbff
frankfanyq <fyq990508@sjtu.edu.cn>
2021-12-01 17:48:57 -0500

    feat: now read env var from .env

--- context.py
+++ context.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 def get_db_info():
     '''
@@ -6,9 +9,9 @@ def get_db_info():
     '''
     
     db_info = {
-        "host": os.environ.get("DBHOST", None),
-        "user": os.environ.get("DBUSER", None),
-        "password": os.environ.get("DBPASSWORD", None),
+        "host": os.getenv("DBHOST"),
+        "user": os.getenv("DBUSER"),
+        "password": os.getenv("DBPASSWORD"),
     }
 
     return db_info
@@ -16,8 +19,8 @@ def get_db_info():
 def get_google_blueprint_info():
 
     google_blueprint_info = {
-        "client_id": os.environ.get("GBPCLIENTID", None),
-        "client_secret": os.environ.get("GBPCLIENTSECRET", None),
+        "client_id": os.getenv("GBPCLIENTID"),
+        "client_secret": os.getenv("GBPCLIENTSECRET"),
     }
     
     return google_blueprint_info
@@ -25,9 +28,9 @@ def get_google_blueprint_info():
 def get_aws_access_key_info():
 
     aws_access_key_info = {
-        "region_name": os.environ.get("AWSREGIONNAME", None),
-        "aws_access_key_id": os.environ.get("AWSACCESSKEYID", None),
-        "aws_secret_access_key": os.environ.get("AWSACCESSKEYSECRET", None)
+        "region_name": os.getenv("AWSREGIONNAME"),
+        "aws_access_key_id": os.getenv("AWSACCESSKEYID"),
+        "aws_secret_access_key": os.getenv("AWSACCESSKEYSECRET")
     }
 
     return aws_access_key_info
